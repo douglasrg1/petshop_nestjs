eeafd0c850badaee19f94c8dd070ac7442cb4269
Douglas <douglasrg1@gmail.com>
2019-10-08 14:39:10 -0300

    connection to BD added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BackofficeModule } from './backoffice/backoffice.module';
+import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [BackofficeModule],
+  imports: [
+    BackofficeModule,
+    MongooseModule.forRoot('mongodb+srv://douglas:38914821@petshop-radix.mongodb.net/test?retryWrites=true&w=majority')
+  ],
   controllers: [],
   providers: [],
 })
