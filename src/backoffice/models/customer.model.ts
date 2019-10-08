25ff80bddf26afdf9559f07564afed9c32f476e6
Douglas <douglasrg1@gmail.com>
2019-10-08 11:42:33 -0300

    added mongoose and user.schema in the project

--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,12 +1,13 @@
 import { PetModel } from "./pet.model";
 import { Addres } from "../valueObjects/address.vo";
 import { CreditCard } from "../valueObjects/creditCard.vo";
+import { User } from "./user";
 
-export class CustomerModel{
-    
-    constructor(public name: string, public document: string, public email: string,public password: string,
-        public active: boolean,public pets: PetModel[],public billingAddress: Addres, shippingAddress: Addres,
-        public creditCard: CreditCard) {
-        
+export class CustomerModel {
+
+    constructor(public name: string, public document: string, public email: string, public pets: PetModel[],
+        public billingAddress: Addres, public shippingAddress: Addres, public creditCard: CreditCard,
+        public user: User) {
+
     }
 }
