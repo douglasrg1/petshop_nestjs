60479c6a88c5627f7e531daa6b85a71d091e00ab
Douglas <douglasrg1@gmail.com>
2019-10-14 14:50:27 -0300

    addded mysql and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import {MongooseModule} from '@nestjs/mongoose'
+import { StoreModule } from './modules/store/store.module';
 
 @Module({
   imports: [
     BackofficeModule,
-    MongooseModule.forRoot('mongodb+srv://douglas:38914821@petshop-radix.mongodb.net/test?retryWrites=true&w=majority')
+    MongooseModule.forRoot('mongodb+srv://douglas:38914821@petshop-radix.mongodb.net/test?retryWrites=true&w=majority'),
+    StoreModule
   ],
   controllers: [],
   providers: [],
