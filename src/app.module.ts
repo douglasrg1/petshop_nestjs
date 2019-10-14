615d753061d1ea134ea50293a8a4a2d626b6b2ba
Douglas <douglasrg1@gmail.com>
2019-10-14 16:58:10 -0300

    added product entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
-import {MongooseModule} from '@nestjs/mongoose'
+import {MongooseModule} from '@nestjs/mongoose';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { StoreModule } from './modules/store/store.module';
+import { from } from 'rxjs';
 
 @Module({
   imports: [
     BackofficeModule,
     MongooseModule.forRoot('mongodb+srv://douglas:38914821@petshop-radix.mongodb.net/test?retryWrites=true&w=majority'),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'teste123',
+      database: '7180',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
     StoreModule
   ],
   controllers: [],
