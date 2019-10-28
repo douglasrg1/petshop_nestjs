bf3e0d2f5774187ad952a7135cc6efcd943b129c
Douglas <douglasrg1@gmail.com>
2019-10-28 14:16:22 -0300

    added swagger in the project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import { CustomLogger } from './shared/services/custom-logger.service';
+import { from } from 'rxjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {logger: new CustomLogger()});
   app.use(compression());
+  //open api(swagger)
+  const options = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API do projeto petshop')
+    .setVersion('1.0.0')
+    .addTag('petshop')
+    .build();
+
+  const document = SwaggerModule.createDocument(app,options);
+  SwaggerModule.setup('docs',app,document); //url//docs
+
   await app.listen(3000);
 }
 bootstrap();
